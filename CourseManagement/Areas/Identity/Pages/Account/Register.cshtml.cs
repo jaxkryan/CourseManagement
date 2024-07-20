28507ee1994277601cbccd0971419b35aebfd6f4
Le Dang Huy <huyg.a22052003@gmail.com>
2024-07-20 16:16:45 +0700

    add register and confirm email
    add register and confirm email

--- CourseManagement/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CourseManagement/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using CourseManagement.Models;
+using CourseManagement.EmailSender;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -18,13 +19,13 @@ namespace CourseManagement.Areas.Identity.Pages.Account
         private readonly SignInManager<WebUser> _signInManager;
         private readonly UserManager<WebUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
+        private readonly ISendMailService _emailSender;
 
         public RegisterModel(
             UserManager<WebUser> userManager,
             SignInManager<WebUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            ISendMailService emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
@@ -74,7 +75,7 @@ namespace CourseManagement.Areas.Identity.Pages.Account
             public string ConfirmPassword { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = "~/")
+        public async Task OnGetAsync(string returnUrl = "/~")
         {
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
@@ -102,34 +103,57 @@ namespace CourseManagement.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
-
-                    await _userManager.AddToRoleAsync(user, "student");
-
-                    var userId = await _userManager.GetUserIdAsync(user);
+                    _logger.LogInformation("Create acc successfully");
+                    // phát sinh token theo thông tin user để xác nhận email
+                    // mỗi user dựa vào thông tin sẽ có một mã riêng, mã này nhúng vào link
+                    // trong email gửi đi để người dùng xác nhận
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+                    // callbackUrl = /Account/ConfirmEmail?userId=useridxx&code=codexxxx
+                    // Link trong email người dùng bấm vào, nó sẽ gọi Page: /Acount/ConfirmEmail để xác nhận
+                    var roles = await _userManager.GetRolesAsync(user);
+
+                    if (roles.Contains("student"))
+                    {
+                        returnUrl = "/Student/Index";
+                    }
+                    else if (roles.Contains("admin"))
+                    {
+                        returnUrl = "/Admin/Index";
+                    }
+                    else if (roles.Contains("teacher"))
+                    {
+                        returnUrl = "/Teacher/Index";
+                    }
+                    else if (roles.Contains("parent"))
+                    {
+                        returnUrl = "/Parent/Index";
+                    }
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
                         pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl},
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    // Gửi email    
+                    await _emailSender.SendEmailAsync(Input.Email, "Xác nhận địa chỉ email",
+                        $"Hãy xác nhận địa chỉ email bằng cách <a href='{callbackUrl}'>Bấm vào đây</a>.");
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    if (_userManager.Options.SignIn.RequireConfirmedEmail)
                     {
-                        TempData["notification"] = "Please check your email to confirm your account.";
-                        TempData["notiType"] = "GREEN";
-                        return RedirectToPage("./Login");
+                        // Nếu cấu hình phải xác thực email mới được đăng nhập thì chuyển hướng đến trang
+                        // RegisterConfirmation - chỉ để hiện thông báo cho biết người dùng cần mở email xác nhận
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
                     }
                     else
                     {
+                        // Không cần xác thực - đăng nhập luôn
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
                     }
                 }
+                // Có lỗi, đưa các lỗi thêm user vào ModelState để hiện thị ở html heleper: asp-validation-summary
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
--- CourseManagement/Program.cs
+++ CourseManagement/Program.cs
@@ -1,7 +1,13 @@
-using CourseManagement.Models;
+﻿using CourseManagement.Models;
+using CourseManagement.EmailSender;
 using CourseManagement.Pages.Service;
+using Microsoft.AspNetCore.Authentication.Cookies;
+//using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,9 +24,60 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDefaultIdentity<WebUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+services.AddOptions();                                        // Kích hoạt Options
+var mailsettings = builder.Configuration.GetSection("MailSettings");
+services.AddTransient<ISendMailService, SendMailService>();
+services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+
+//services.AddAuthentication()
+//    .AddGoogle(googleOptions =>
+//    {
+//        IConfigurationSection googleAuthNSection = configuration.GetSection("Authentication:Google");
+
+//        googleOptions.ClientId = googleAuthNSection["ClientId"];
+//        googleOptions.ClientSecret = googleAuthNSection["ClientSecret"];
+//        googleOptions.CallbackPath = "/google-login";
+
+//    });
+services.Configure<IdentityOptions>(options => {
+    // Thiết lập về Password
+    options.Password.RequireDigit = false; // Không bắt phải có số
+    options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+    options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+    options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+    options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+    options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+    // Cấu hình Lockout - khóa user
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // Khóa 5 phút
+    options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
+    options.Lockout.AllowedForNewUsers = true;
+
+    // Cấu hình về User.
+    options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+    // Cấu hình đăng nhập.
+    options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+    options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+    options.SignIn.RequireConfirmedAccount = true;
+});
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+//}).AddCookie()
+//.AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
+//{
+//    options.ClientId = builder.Configuration.GetSection("Google: ClientId").Value;
+//    options.ClientSecret = builder.Configuration.GetSection("Google: ClientSecret").Value;
+//});
 var app = builder.Build();
-
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -28,6 +85,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -40,3 +98,6 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+
+
