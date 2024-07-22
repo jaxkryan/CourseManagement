37ba1c1f6aac367f1a0bc35490391eff8d57cb2c
Le Dang Huy <huyg.a22052003@gmail.com>
2024-07-22 21:06:53 +0700

    add redirect filter

---
+++ CourseManagement/Areas/RedirectFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CourseManagement.Areas
+{
+    public class RedirectFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (context.HttpContext.User.Identity.IsAuthenticated &&
+                context.HttpContext.User.IsInRole("admin"))
+            {
+                if (context.HttpContext.Request.Path == "/Index" ||
+                    context.HttpContext.Request.Path == "/")
+                {
+                    context.Result = new RedirectResult("/Admin/Index");
+                }
+            }
+            if (context.HttpContext.User.Identity.IsAuthenticated &&
+                context.HttpContext.User.IsInRole("teacher"))
+            {
+                if (context.HttpContext.Request.Path == "/Index" ||
+                    context.HttpContext.Request.Path == "/")
+                {
+                    context.Result = new RedirectResult("/Teacher/Index");
+                }
+            }
+        }
+    }
+}
