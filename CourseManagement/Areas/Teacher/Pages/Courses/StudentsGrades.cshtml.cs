0be79a622401cb480ea7c5040f5bb8dc803db667
Luutu011 <luututkls2@gmail.com>
2024-07-23 01:20:24 +0700

    export grade to excels
    export grade to excels

--- CourseManagement/Areas/Teacher/Pages/Courses/StudentsGrades.cshtml.cs
+++ CourseManagement/Areas/Teacher/Pages/Courses/StudentsGrades.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 
 namespace CourseManagement.Areas.Teacher.Pages.Courses
 {
@@ -50,7 +51,8 @@ namespace CourseManagement.Areas.Teacher.Pages.Courses
 
                 StudentGrades = users.ToDictionary(
                     u => $"{u.FirstName} {u.LastName}",
-                    u => assignmentsInCourse.Select(a => {
+                    u => assignmentsInCourse.Select(a =>
+                    {
                         var submission = submissions.FirstOrDefault(s => s.AssignmentId == a.AssignmentId && s.UserId == u.Id);
                         return (a.AssignmentTitle, submission?.Grade);
                     }).ToList()
@@ -62,5 +64,100 @@ namespace CourseManagement.Areas.Teacher.Pages.Courses
         {
             return RedirectToPage(new { id = CourseId });
         }
+        public async Task<IActionResult> OnPostExportToExcelAsync(int courseId)
+        {
+            // Update courseId
+            CourseId = courseId;
+
+            // Fetch student grades for the course
+            await GetStudentGradesAsync(CourseId);
+
+            // Export to Excel
+            return await ExportToExcelAsync();
+        }
+        public async Task<IActionResult> ExportToExcelAsync()
+        {
+            // Fetch student grades for the course
+            await GetStudentGradesAsync(CourseId);
+
+            // Check if there are grades to export
+            if (StudentGrades == null || !StudentGrades.Any())
+            {
+                return NotFound("No grades available for export.");
+            }
+
+            // Create a new Excel package
+            using (var package = new ExcelPackage())
+            {
+                // Add a worksheet to the package
+                var worksheet = package.Workbook.Worksheets.Add("Student Grades");
+
+                // Add headers
+                worksheet.Cells[1, 1].Value = "Student";
+                worksheet.Cells[1, 2].Value = "Assignment";
+                worksheet.Cells[1, 3].Value = "Grade";
+
+                var row = 2;
+
+                // Add data rows
+                foreach (var student in StudentGrades)
+                {
+                    var assignments = student.Value;
+                    foreach (var assignment in assignments)
+                    {
+                        worksheet.Cells[row, 1].Value = student.Key;
+                        worksheet.Cells[row, 2].Value = assignment.AssignmentTitle;
+                        worksheet.Cells[row, 3].Value = assignment.Grade.HasValue ? assignment.Grade.Value.ToString("F2") : "N/A";
+                        row++;
+                    }
+                }
+
+                // Prepare the file for download
+                var stream = new MemoryStream();
+                package.SaveAs(stream);
+                stream.Position = 0;
+
+                // Return the file
+                var fileName = $"StudentGrades_{CourseId}.xlsx";
+                return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+            }
+        }
+
+        private async Task GetStudentGradesAsync(int courseId)
+        {
+            // Fetch students enrolled in the course
+            var studentsInCourse = await _context.Enrollments
+                .Where(e => e.CourseId == courseId)
+                .Select(e => e.UserId)
+                .ToListAsync();
+
+            // Fetch assignments for the course
+            var assignmentsInCourse = await _context.Assignments
+                .Where(a => a.CourseId == courseId)
+                .ToListAsync();
+
+            // Fetch submissions related to the assignments
+            var submissions = await _context.Submissions
+                .Where(s => assignmentsInCourse.Select(a => a.AssignmentId).Contains(s.AssignmentId))
+                .ToListAsync();
+
+            // Fetch user details for the students
+            var users = await _context.Users
+                .Where(u => studentsInCourse.Contains(u.Id))
+                .ToListAsync();
+
+            // Map students to their grades
+            StudentGrades = users.ToDictionary(
+                u => $"{u.FirstName} {u.LastName}",
+                u => assignmentsInCourse.Select(a =>
+                {
+                    var submission = submissions.FirstOrDefault(s => s.AssignmentId == a.AssignmentId && s.UserId == u.Id);
+                    return (a.AssignmentTitle, submission?.Grade);
+                }).ToList()
+            );
+        }
+
+
+
     }
 }
